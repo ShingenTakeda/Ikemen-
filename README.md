97f5c2c64d278b278a15b0d13099a03635297947
Gustavo Takeda <gustakeda@gmail.com>
2020-03-30 23:08:17 -0300

    Common header and basic readme stuff

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Ikemen-
+# Ikemen-PP
+
 A cpp port of Ikemen-go
+
+## Building
+
+To build Ikemen-PP you will need the conan package manager and premake5,
+then run a "conan install ." in the top directory followed by "premake5 "your-tool-of-choice" "
+
+### Running
+
+All executables will be under the bin directory
+
+## Licence
+
+[MIT Licence](LICENSE.txt)
--- src/Common.hpp
+++ src/Common.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <string>
 #include <iostream>
 #include <cstdint>
@@ -13,10 +15,19 @@ class Common
         */
 
         int32_t Random();
+        int Srand(int32_t s);
+        int32_t Rand(int32_t min, int32_t max);
+        int32_t RandI(int32_t min, int32_t max);
+        
+        //Make this two funnctions type float 32 bits
+        _Float32 RandF();
+        _Float32 RandF32();
 
         ///SearchFile returns the directory that file is located
         ///This search on deffile directory, then it keep looking on other dirs
         std::string SearchFile(std::string file, std::string deffile);
+
+
 
     private:
         int Max(int32_t max);
